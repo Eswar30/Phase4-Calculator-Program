8c5799492410bad28d8b2bfad1ca0273b900c30a
eswar ayya <eswarayya30gs@gmail.com>
2022-08-25 10:39:52 +0000

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Mul(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+}
